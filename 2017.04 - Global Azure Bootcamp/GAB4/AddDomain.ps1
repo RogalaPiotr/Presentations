94ab0db5c6e70f5553b9177f798028e6803ce5c5
Piotr Rogala <71rogi@gmail.com>
2022-02-08 20:46:02 +0100

    update presentations

---
+++ 2017.04 - Global Azure Bootcamp/GAB4/AddDomain.ps1
@@ -0,0 +1,1 @@
+$secpasswd = ConvertTo-SecureString “HASLO” -AsPlainText -Force; $mycreds = New-Object System.Management.Automation.PSCredential (“DOMAIN\LOGIN”, $secpasswd); add-computer –domainname DOMAIN -Credential $mycreds -restart –force
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Presentations
-Presentations prepared by me for community events.
+Here you can find presentations that I have prepared or people who participated in the events that I have organized.
